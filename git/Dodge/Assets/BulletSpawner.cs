398e4d749cbaf59b10ba03caab6cb5b4534724d1
songsesoon <sss010201sss@naver.com>
2023-04-21 12:45:09 +0900

    2023.04.21
    fsadfasdf

---
+++ git/Dodge/Assets/BulletSpawner.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bulletPrefab;//생성할 탄알의 원본 ㅍ츠리팹
+    public float spawnRateMin = 0.5f; //최소 생성 주기
+    public float spawnRateMax = 3f;//최대 생성주기
+
+    private Teansform target;//발사할 대상
+    private float ;//생성주기
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
