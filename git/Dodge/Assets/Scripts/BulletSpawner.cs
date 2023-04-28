8ebc3458af1072a59a27155fa4f3c4e3a4c4fe73
songsesoon <sss010201sss@naver.com>
2023-04-28 12:38:47 +0900

    2023.04.28
    ajsnfkasf

--- git/Dodge/Assets/Scripts/BulletSpawner.cs
+++ git/Dodge/Assets/Scripts/BulletSpawner.cs
@@ -14,7 +14,7 @@ public class BulletSpawner : MonoBehaviour
         //최근 생성 이후의 누적 시간을 0으로 초기화
         timeAfterSpawn = 0f;
         //탄알 생성 간격을 spawnRateMin과 spawnRateMax 사이에서 랜덤 지정
-        spawnRateMax = Random.Range(spawnRateMin, spawnRateMax);
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
         //PlayerDontroller 컴포넌트를 가진 게임 오브젝트를 찾아 조준 대상으로 설정
         target = FindObjectOfType<PlayerController>().transform;
     }
--- git/Dodge/Assets/Scripts/GameManager.cs
+++ git/Dodge/Assets/Scripts/GameManager.cs
@@ -6,8 +6,8 @@ using UnityEngine.SceneManagement; //씬 관리 관련 라이브러리
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject gameoverText; // 게임오버 시 활성화 할 텍스트 게임 오브젝트
-    public Text timeText; // 생존 시간
+    public GameObject gameoverText; // 게임오버 시 활성화할 텍스트 게임 오브젝트
+    public Text timeText; // 생존 시간을 표시할 텍스트 컴포넌트
     public Text recordText; // 최고 기록을 표시할 텍스트 컴포넌트
 
     private float surviveTime; // 생존 시간
@@ -15,10 +15,11 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        // 생존 시간과 게임어버 상태의 초기화
+        // 생존 시간과 게임오버 상태의 초기화
         surviveTime = 0;
         isGameover = false;
     }
+
     void Update()
     {
         // 게임오버가 아닌 동안
@@ -27,7 +28,7 @@ public class GameManager : MonoBehaviour
             // 생존 시간 갱신
             surviveTime += Time.deltaTime;
             // 갱신한 생존 시간을 timeText 텍스트 컴포넌트를 이용해 표시
-            timeText.text = "Time : " + (int)surviveTime;
+            timeText.text = "Time: " + (int)surviveTime;
         }
         else
         {
@@ -41,7 +42,8 @@ public class GameManager : MonoBehaviour
     }
 
     //현재 게임을 게임오버 상태로 변경하는 메서드
-    public void EndGame() {
+    public void EndGame()
+    {
         //현재 상태를 게임오버 상태로 전환
         isGameover = true;
         // 게임오버 텍스트 게임 오브젝트를 활성화
@@ -53,12 +55,13 @@ public class GameManager : MonoBehaviour
         //이전까지의 최고 기록보다 현재 생존 시간이 더 크다면
         if (surviveTime > bestTime)
         {
-            //최고 기록 값을 현재 생존 시간이 더 크다면
+            //최고 기록 값을 현재 생존 시간 값으로 변경
             bestTime = surviveTime;
             //변경된 최고 기록을 BestTime 키로 저장
-            PlayerPrefs.SetFloat("BestTime,bestTime);
+            PlayerPrefs.SetFloat("BestTime", bestTime);
         }
 
         //최고 기록을 recordText 텍스트 컴포넌트를 이용해 표시
         recordText.text = "Best Time" + (int)bestTime;
     }
+}
